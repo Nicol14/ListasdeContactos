88f6fb0dab64de2ca22b4aba209864cfc0daa901
Nicol Munevar <nicolmunevar14@gmail.com>
2022-06-22 21:30:03 -0500

    añadir a mis remopositorios nueva lista de contactos

---
+++ Lista.js
@@ -0,0 +1,29 @@
+const listaDeContactos = [{}, {}, {}];
+
+listaDeContactos = [
+  { id: 1, nombres: "Nicol Dayana", apellidos: "Munevar Trujillo" },
+  { id: 1, nombres: "Claudia Patricia", apellidos: "Trujillo Gualteros" },
+  { id: 1, nombres: "Miguel Ricardo", apellidos: "Leon Trujillo" },
+  { id: 1, nombres: "Angel German", apellidos: "Torres Velez" },
+  { id: 1, nombres: "Karen Natalia", apellidos: "Guzman Munevar" },
+  { id: 1, nombres: "Anyul Sofia", apellidos: "Carrillo Morales" },
+  { id: 1, nombres: "Lian David", apellidos: "Muñoz Carrillo" },
+  { id: 1, nombres: "Maria Jose", apellidos: "Molano Aragon" },
+  { id: 1, nombres: "Yimmy Alfredo", apellidos: "Munevar Rivera" },
+  { id: 1, nombres: "Emmily Fernanda", apellidos: "Torres Velez" },
+];
+
+function agregar() {
+  listaDeContactos.push;
+  console.log;
+}
+
+function sacar() {
+  listaDeContactos.pop = listaDeContactos.length(9);
+  console.log;
+}
+
+function imprimir() {
+  listaDeContactos;
+  console.log;
+}

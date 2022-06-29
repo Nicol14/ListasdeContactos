7566c852a6ae4106521f864834003eccd91c2160
Nicol Munevar <nicolmunevar14@gmail.com>
2022-06-28 20:58:48 -0500

    Falta terminar la lista de contactos

--- Lista.js
+++ Lista.js
@@ -2,15 +2,15 @@ const listaDeContactos = [{}, {}, {}];
 
 listaDeContactos = [
   { id: 1, nombres: "Nicol Dayana", apellidos: "Munevar Trujillo" },
-  { id: 1, nombres: "Claudia Patricia", apellidos: "Trujillo Gualteros" },
-  { id: 1, nombres: "Miguel Ricardo", apellidos: "Leon Trujillo" },
-  { id: 1, nombres: "Angel German", apellidos: "Torres Velez" },
-  { id: 1, nombres: "Karen Natalia", apellidos: "Guzman Munevar" },
-  { id: 1, nombres: "Anyul Sofia", apellidos: "Carrillo Morales" },
-  { id: 1, nombres: "Lian David", apellidos: "Muñoz Carrillo" },
-  { id: 1, nombres: "Maria Jose", apellidos: "Molano Aragon" },
-  { id: 1, nombres: "Yimmy Alfredo", apellidos: "Munevar Rivera" },
-  { id: 1, nombres: "Emmily Fernanda", apellidos: "Torres Velez" },
+  { id: 2, nombres: "Claudia Patricia", apellidos: "Trujillo Gualteros" },
+  { id: 3, nombres: "Miguel Ricardo", apellidos: "Leon Trujillo" },
+  { id: 4, nombres: "Angel German", apellidos: "Torres Velez" },
+  { id: 5, nombres: "Karen Natalia", apellidos: "Guzman Munevar" },
+  { id: 6, nombres: "Anyul Sofia", apellidos: "Carrillo Morales" },
+  { id: 7, nombres: "Lian David", apellidos: "Muñoz Carrillo" },
+  { id: 8, nombres: "Maria Jose", apellidos: "Molano Aragon" },
+  { id: 9, nombres: "Yimmy Alfredo", apellidos: "Munevar Rivera" },
+  { id: 10, nombres: "Emmily Fernanda", apellidos: "Torres Velez" },
 ];
 
 function agregar() {
